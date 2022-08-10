7211aa449590b082fcbe1c6c3d5bde983e0dc419
AakashGhosh7044384754 <109957580+AakashGhosh7044384754@users.noreply.github.com>
2022-08-10 17:20:22 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,64 @@
 const express = require('express');
 const router = express.Router();
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ],
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ],
-       },
-   ]
 
-   router.post('/players', function (req, res) {
-        let abc=req.body
-        players.push(abc)
-        console.log(req.body)
-       res.send(  { data: players , status: true }  )
-   })
+let players =[
+    {
+        "name":"manish",
+        "dob" :"1/1/1995",
+        "gender":"male",
+        "city":"jalandhar",
+        "sports":[
+            "swimming"
+        ],
+    },
+     
+    {
+        "name":"gopal",
+        "dob" :"1/09/1995",
+         "gender": "male",
+         "city" :"delhi",
+         "sports":[
+            "soccer"
+         ],
+    },
+      
+     {
+         "name":"lokesh",
+         "dob" :"1/1/1990",
+         "gender":"male",
+          "city":"mumbai",
+          "sports":[
+            "soccer"
+          ],
+          
+        },
+        
+    ]
+
+   router.post('/players', function(req, res){
+
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    
+     for(let i=0; i<players.length; i++){
+        if(players[i].name == newPlayersName){
+            isNameRepeated = true;
+            break;
+        }
+     }
+
+            if(isNameRepeated == true){
+                res.send(' name already exixts')
+            }else
+            {
+                players.push(newPlayer)
+                res.send(players)
+            }
+            
+    })
+
+
   
 module.exports = router;
