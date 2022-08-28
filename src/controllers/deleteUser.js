7067d30be0cc7e2b9ce1bf77de80209648e083fb
AakashGhosh7044384754 <109957580+AakashGhosh7044384754@users.noreply.github.com>
2022-08-28 23:54:55 +0400

    Update deleteUser.js

--- src/controllers/deleteUser.js
+++ src/controllers/deleteUser.js
@@ -2,6 +2,7 @@ const userModel = require('../models/userModel')
 
 
 const deleteDetails = async function (req, res) {
+    let userId=req.params.userId
     let update = await userModel.findByIdAndUpdate({ _id: userId }, { isDeleted: true }, { new: true })
     res.send(update)
 }
