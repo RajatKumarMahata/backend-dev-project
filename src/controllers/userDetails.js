1a9a264c61c7acdea9254a26f0deeaaceee680b1
AakashGhosh7044384754 <109957580+AakashGhosh7044384754@users.noreply.github.com>
2022-08-28 23:52:27 +0530

    Update userDetails.js

--- src/controllers/userDetails.js
+++ src/controllers/userDetails.js
@@ -2,6 +2,8 @@ const userModel = require('../models/userModel')
 
 
 const userDetails = async function (req, res) {
+    let userId = req.params.userId
+    let getDetails = await userModel.findById(userId)
     res.send(getDetails)
 }
 
