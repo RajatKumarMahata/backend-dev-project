1d9a686ce668ca1d57350ff633927ccb8070bb77
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-08-29 23:57:57 +0530

    opjk

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,27 +1,31 @@
-const authenticate = async function(req, res, next) {
-    //check the token in request header
-    //validate this token
-    let token = req.headers["x-auth-token"]
-    if (!token) { return res.send("Header is missing") }
-    let decodedToken = jwt.verify(token, "I am learning the creation of jwt")
-    if (!decodedToken) { return res.send("Not a valid token") }
-    let userId = req.params.userId
-    let idCheck = mongoose.Types.ObjectId.isValid(userId)
-    if (!idCheck) { return res.send("Not a valid userID") }
-    let check = await userModel.findById(userId)
-    if (!check) { return res.send("No such user exist") }
-    next()
-}
+const authenticate = async function (req, res, next) {
+  let token = req.headers["x-auth-token"];
+  if (!token) {
+    return res.send("Header is missing");
+  }
+  let decodedToken = jwt.verify(token, "I am learning the creation of jwt");
+  if (!decodedToken) {
+    return res.send("Not a valid token");
+  }
+  let userId = req.params.userId;
+  let idCheck = mongoose.Types.ObjectId.isValid(userId);
+  if (!idCheck) {
+    return res.send("Not a valid userID");
+  }
+  let check = await userModel.findById(userId);
+  if (!check) {
+    return res.send("No such user exist");
+  }
+  next();
+};
 
+const authorise = function (req, res, next) {
+  let requestedUserId = req.params.userId;
+  if (requestedUserId !== req.loggedInUser) {
+    res.send({ status: false, msg: "permission denied" });
+  }
+  next();
+};
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    let requestedUserId= req.params.userId
-    if (requestedUserId!==req.loggedInUser){
-        res.send({status:false, msg:"permission denied"})
-    }
-    next()
-}
-
-module.exports.authenticate=authenticate
-module.exports.authorise=authorise
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController= require("../controllers/userController")
-const auth=require("../middleware/auth")
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
-router.post("/users",auth.authenticate, userController.createUser)
+router.post("/users", auth.authenticate, userController.createUser);
 
-router.post("/login",auth.authenticate, userController.loginUser)
+router.post("/login", auth.authenticate, userController.loginUser);
 
-//The userId is sent by front end
-router.get("/users/:userId",auth.authorise, userController.getUserData)
-router.post("/users/:userId/posts",auth.authorise, userController.postMessage)
+router.get("/users/:userId", auth.authorise, userController.getUserData);
+router.post("/users/:userId/posts", auth.authorise, userController.postMessage);
 
-router.put("/users/:userId",auth.authorise, userController.updateUser)
-router.delete('/users/:userId',auth.authorise, userController.deleteUser)
+router.put("/users/:userId", auth.authorise, userController.updateUser);
+router.delete("/users/:userId", auth.authorise, userController.deleteUser);
 
 module.exports = router;
