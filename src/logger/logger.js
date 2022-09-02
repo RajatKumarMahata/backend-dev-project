1e76ca9575f7af445ae4c94c8662cebc1353b337
rajatkumarmahata7@gmail.com <you@example.com>
2022-09-03 01:59:41 +0530

    ok

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,4 +1,4 @@
 let Welcome =function(){
-    console.log("Welcome to my application. I am AAKASH and a part of FunctionUp Plutonium cohort", Welcome)
+    console.log("Welcome to my application. I am raj and a part of FunctionUp Plutonium cohort", Welcome)
 }
 module.exports.welMs=Welcome
