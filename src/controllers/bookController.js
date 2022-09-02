1f4ad018d3d591d331c0b099e11bfefcbc696f44
rajatkumarmahata7@gmail.com <you@example.com>
2022-09-03 03:35:52 +0530

    ok

---
+++ src/controllers/bookController.js
@@ -0,0 +1,53 @@
+const authorModel = require("../models/authorModel")
+const bookModel= require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
+
+const createBooks= async function (req, res) {
+    let book = req.body
+   
+    let authorid=book.author 
+   
+    let publisherid=book.publisher
+
+    let checkAuthorid=await authorModel.find({_id:authorid})
+
+     let checkPublisherid= await publisherModel.find({_id:publisherid})
+     
+     if(!authorid){
+        res.send("Author is REQUIRED!")
+     }  else if (!checkAuthorid[0]){
+        res.send("Please give a valid Auther id, auther is not present!")
+     }else if (!publisherid){
+        res.send(" This field (Publisher) is REQUIRED!")
+     }else if (!checkPublisherid[0]){
+        res.send("Please give a valid Publisher id, publisher is not present!")
+      }
+    else {
+        let bookCreated= await bookModel.create(book)
+        res.send({msg: bookCreated})
+     }
+}    
+
+const getBookData= async function (req, res) {
+    let books = await bookModel.find()
+    res.send({data: books})
+}
+
+const getBooksWithAuthorDetails = async function (req, res) {
+    let allBook = await bookModel.find().populate(['author','publisher'])
+    res.send({data: allBook})
+
+}
+// const updateBooks=async function (req,res){
+//     let updateB=await publisherModel.find({$in:[{name:'penguin'},{name:'HarperCollins'}]})
+//    let updatA= await authorModel.find({$in:{rating:{$gt:3.5}}})
+//    if (updateB){
+//     bookModel.updateMany({isHardCovered:true})
+//    }
+// }
+
+
+module.exports.createBook= createBooks
+module.exports.getBookData= getBookData
+module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+ 
